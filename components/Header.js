3efea712a1a7fe452a6c82c077514e21e901b745
Guxim <guxim.selmani1@hotmail.com>
2021-09-06 03:07:16 +0200

    Shop

--- components/Header.js
+++ components/Header.js
@@ -6,18 +6,22 @@ import {
     Text,
     View,
     TouchableOpacity,
-    Alert
+    Alert,
+    Image
 } from 'react-native';
 
 import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
 const Header = () => {
 
     return (
-        <View>
-            <TouchableOpacity activeOpacity={1} onPress={() => { Alert.alert("You pressed the menu") }}>
-                <FontAwesome5 name="grip-lines" size={20} style={{ margin: 5 }} />
+        <View style={{ margin: 10, flex: 1, flexDirection: 'row' }}>
+            <TouchableOpacity style={{ width: 20, }} onPress={() => { Alert.alert("You pressed the menu") }}>
+                <FontAwesome5 name="bars" size={25} color={'#3d3d3d'} >
+                </FontAwesome5>
             </TouchableOpacity>
-        </View>
+            <Image source={require('../images/Shop_logo.png')} style={{ width: 35, height: 25, marginLeft: 10 }} />
+            <Text style={{ fontSize: 21, fontWeight: 'bold', color: '#3d3d3d', marginLeft: 5 }}>SHOP</Text>
+        </View >
     );
 }
 
