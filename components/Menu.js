7e9a849723c3a9f724cdd6efe98ab15df3a0a034
Guxim <guxim.selmani1@hotmail.com>
2021-09-08 17:19:38 +0200

    Shop

---
+++ components/Menu.js
@@ -0,0 +1,20 @@
+/* eslint-disable */
+import React, { useState } from 'react';
+
+import {
+    StyleSheet,
+    Text,
+    View,
+    TouchableOpacity,
+    Alert,
+    Image,
+} from 'react-native';
+const Menu = () => {
+    return (
+        <View style={{ flex: 1, position: 'absolute', borderWidth: 5 }}>
+            <Text>!!!!!!!!!!!!!!!!!!!!!!!!!!!!!</Text>
+        </View>
+    );
+}
+
+export default Menu;
